29746495fa169a8e2922383f7b727f8b349fff78
Barath Ramesh <barath.ramesh@ti.com>
2020-10-09 17:26:15 -0500

    Converted tabs to spaces in init file

--- init.el
+++ init.el
@@ -205,8 +205,8 @@
 (require 'tramp)
 (setq tramp-default-method "ssh")
 (tramp-set-completion-function "ssh"
-			                      '((tramp-parse-sconfig "/etc/ssh_config")
-				                     (tramp-parse-sconfig "~/.ssh/config")))
+                                  '((tramp-parse-sconfig "/etc/ssh_config")
+                                     (tramp-parse-sconfig "~/.ssh/config")))
 ;; (customize-set-variable 'tramp-syntax 'simplified)
 
 ;; Automatically updated files modified outside of emacs
